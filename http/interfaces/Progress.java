bf90cefe12dd3a2c2e11b2a31f3fd6ade8e6d5f5
T M <tm_private@163.com>
2018-03-21 23:29:38 +0800

    delete http

--- http/interfaces/Progress.java
+++
@@ -1,9 +0,0 @@
-package com.tm.book_of_exercises.main.http.interfaces;
-
-/**
- * Created by Administrator on 2016/12/21.
- */
-@FunctionalInterface
-public interface Progress {
-    void progress(float p);
-}
--- http/interfaces/Success.java
+++
@@ -1,9 +0,0 @@
-package com.tm.book_of_exercises.main.http.interfaces;
-
-/**
- * Created by 耿 on 2016/8/15.
- */
-@FunctionalInterface
-public interface Success {
-    void Success(String model);
-}
